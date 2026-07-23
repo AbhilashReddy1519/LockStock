2a10307d570a779630e4997cfde88d391f404354
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2026-07-23 10:30:33 +0530

    feat: add compression and logging middleware, implement error handling and health check endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,35 @@
-import express, {Express} from "express";
+import express, { Express, Response } from "express";
+import cors from 'cors';
+import helmet from "helmet";
+import compression from "compression";
+import morgan from "morgan";
 
+import routes from "./routes/routes.js";
+import { notFoundHandler } from "./middleware/notFound.middleware.js";
+import { errorHandler } from "./middleware/error.middleware.js";
 
 const app: Express = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(compression());
+app.use(morgan("dev"));
+
+app.get("/health", (_, res:Response) => {
+  res.status(200).json({
+    success: true,
+    service: "Inventory Service",
+    status: "running....",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+
+app.use("/api/v1", routes);
+app.use(notFoundHandler);
+// Global Error Handler
+app.use(errorHandler);
+
+export default app;
---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./app.js";
+import { env } from "./config/env.js";
+
+const PORT = env.PORT;
+
+const server = app.listen(PORT, () => {
+  console.log(`
+    ===================================
+    Inventory Service Started
+    Environment : ${process.env.NODE_ENV || "development"}
+    Port        : ${PORT}
+    ===================================
+  `);
+});
+
+
+// Graceful Shutdown
+process.on("SIGINT", () => {
+    console.log("Stopping server...");
+    server.close(() => {
+        console.log("Server stopped.");
+        process.exit(0);
+    });
+});
+
+process.on("SIGTERM", () => {
+    console.log("Stopping server...");
+    server.close(() => {
+        console.log("Server stopped.");
+        process.exit(0);
+    });
+});
